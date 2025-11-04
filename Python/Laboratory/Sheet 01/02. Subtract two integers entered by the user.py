187fad10cec99ee0cf4350fe74b612b7e0a4c9f9
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:17:18 +0530

    subtract two integers

---
+++ Python/Laboratory/Sheet 01/02. Subtract two integers entered by the user.py
@@ -0,0 +1,5 @@
+# 2. Subtract two integers entered by the user.
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+difference = num1 - num2
+print("The difference between", num1, "and", num2, "is", difference)
