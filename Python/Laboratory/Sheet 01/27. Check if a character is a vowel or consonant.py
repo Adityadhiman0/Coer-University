a7749b9ef65063434c8f2e135275b20620c148da
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 00:09:02 +0530

    vowel/consonant checker

---
+++ Python/Laboratory/Sheet 01/27. Check if a character is a vowel or consonant.py
@@ -0,0 +1,10 @@
+# 27. Check if a character is a vowel or consonant.
+char = input("Enter a single alphabet character: ").lower()
+# Check if input is a single alphabet letter
+if len(char) == 1 and char.isalpha():
+    if char in 'aeiou':
+        print("The character is a vowel.")
+    else:
+        print("The character is a consonant.")
+else:
+    print("Invalid input. Please enter a single alphabet letter.")
