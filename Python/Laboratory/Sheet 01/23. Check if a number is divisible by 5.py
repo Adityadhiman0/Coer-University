1edbd9ce31fa341df8dd327a3417d74dd35a6de0
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 00:02:53 +0530

    check divisibility by 5

---
+++ Python/Laboratory/Sheet 01/23. Check if a number is divisible by 5.py
@@ -0,0 +1,7 @@
+# 23. Check if a number is divisible by 5.
+num = int(input("Enter an integer: "))
+# Check divisibility by 5
+if num % 5 == 0:
+    print("The number is divisible by 5.")
+else:
+    print("The number is not divisible by 5.")
