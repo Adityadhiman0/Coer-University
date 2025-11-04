72754015a7c6bfb43bf566acd4c8d943bd64aff9
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 00:03:54 +0530

    leap year

---
+++ Python/Laboratory/Sheet 01/24. Check if a given year is a leap year.py
@@ -0,0 +1,7 @@
+# 24. Check if a given year is a leap year.
+year = int(input("Enter a year: "))
+# Check leap year conditions
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
