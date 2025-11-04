6b43eaedde383a20e643c4f022ae198e08fd379d
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:39:58 +0530

    convert kilometers to miles

---
+++ Python/Laboratory/Sheet 01/15. Convert kilometers to miles.py
@@ -0,0 +1,8 @@
+# 15. Convert kilometers to miles.
+kilometers = float(input("Enter distance in kilometers: "))
+# Conversion factor
+conversion_factor = 0.621371
+# Convert to miles
+miles = kilometers * conversion_factor
+# Display the result
+print("Distance in miles:", miles)
