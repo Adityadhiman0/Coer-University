a48c6a06531fcbfcb86084dc7061d057b9da5ba9
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:18:38 +0530

    multiplication

---
+++ Python/Laboratory/Sheet 01/03. Multiply two integers entered by the user.py
@@ -0,0 +1,6 @@
+# 3. Multiply two integers entered by the user.
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+product = num1 * num2
+print("The product of", num1, "and", num2, "is", product)
