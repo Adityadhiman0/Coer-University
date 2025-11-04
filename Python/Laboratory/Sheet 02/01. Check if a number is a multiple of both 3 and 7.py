645a696fbbc9d21554598d3a812cf5ac61dcd736
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 00:21:48 +0530

    Create 01. Check if a number is a multiple of both 3 and 7.py

---
+++ Python/Laboratory/Sheet 02/01. Check if a number is a multiple of both 3 and 7.py
@@ -0,0 +1,8 @@
+# 1. Check if a number is a multiple of both 3 and 7.
+num = int(input("Enter a number: "))
+
+# Check if the number is divisible by both 3 and 7
+if num % 3 == 0 and num % 7 == 0:
+    print("The number is a multiple of both 3 and 7.")
+else:
+    print("The number is not a multiple of both 3 and 7.")
