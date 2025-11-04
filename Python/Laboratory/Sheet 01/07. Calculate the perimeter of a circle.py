56ac9d92fc2d4ccbf021a7b38b4d1315fe694bbb
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:26:24 +0530

    calculate the perimeter of a circle

---
+++ Python/Laboratory/Sheet 01/07. Calculate the perimeter of a circle.py
@@ -0,0 +1,5 @@
+# 7. Calculate the perimeter of a circle.
+import math
+radius = float(input("Enter the radius of the circle: "))
+perimeter = 2 * math.pi * radius
+print("The perimeter (circumference) of the circle is:", perimeter)
