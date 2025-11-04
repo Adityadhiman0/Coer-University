395826101c0f88efeb8a8835fe63dcb26cc18b47
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:15:43 +0530

    addition program

---
+++ Python/Laboratory/Sheet 01/01. Add two integers entered by the user.py
@@ -0,0 +1,5 @@
+# 1. Add two integers entered by the user.
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
