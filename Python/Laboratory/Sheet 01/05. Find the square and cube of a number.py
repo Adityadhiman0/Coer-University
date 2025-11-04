ab980752e4e9ccd1f3a1689dc7b91def3ed9b0ae
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-04 23:23:20 +0530

    square and cube of a number

---
+++ Python/Laboratory/Sheet 01/05. Find the square and cube of a number.py
@@ -0,0 +1,7 @@
+# 5. Find the square and cube of a number.
+
+num = int(input("Enter an integer: "))
+square = num ** 2
+cube = num ** 3
+print("Square of", num, "is", square)
+print("Cube of", num, "is", cube)
