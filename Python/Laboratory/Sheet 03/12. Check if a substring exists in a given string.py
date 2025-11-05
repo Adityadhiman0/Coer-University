243c906034dea159e644ac47247c95997a32bde6
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 16:42:38 +0530

    Create 12. Check if a substring exists in a given string.py

---
+++ Python/Laboratory/Sheet 03/12. Check if a substring exists in a given string.py
@@ -0,0 +1,7 @@
+main_string = input("Enter the main string: ")
+substring = input("Enter the substring to check: ")
+
+if substring in main_string:
+    print("The substring exists in the main string.")
+else:
+    print("The substring does not exist in the main string.")
