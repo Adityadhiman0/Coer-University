d292c14379bbda592f4ba0d214cc99f65e0fc828
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 16:56:49 +0530

    Separate even and odd numbers from a list.py

---
+++ Python/Laboratory/Sheet 03/30. Separate even and odd numbers from a list.py
@@ -0,0 +1,13 @@
+numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+even_numbers = []
+odd_numbers = []
+
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+    else:
+        odd_numbers.append(num)
+
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
