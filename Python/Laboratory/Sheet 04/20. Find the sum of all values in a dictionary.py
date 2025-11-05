e385b0bb2f04939e5f78c106125c0cba18e8c3d1
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 17:21:36 +0530

    Find the sum of all values in a dictionary.py

---
+++ Python/Laboratory/Sheet 04/20. Find the sum of all values in a dictionary.py
@@ -0,0 +1,13 @@
+# Sample dictionary
+student_marks = {
+    "Amit": 85,
+    "Neha": 92,
+    "Rahul": 78,
+    "Simran": 90,
+    "Priya": 88
+}
+
+# Calculate the sum of all values
+total_marks = sum(student_marks.values())
+
+print("Total sum of marks:", total_marks)
