4e075ad8c67c011d42c75d0c3d06daeb8fbeb654
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 16:32:59 +0530

    Count the number of vowels and consonants

---
+++ Python/Laboratory/Sheet 03/02. Count the number of vowels and consonants in a string.py
@@ -0,0 +1,16 @@
+text = input("Enter a string: ")
+text = text.lower()
+
+vowels = "aeiou"
+vowel_count = 0
+consonant_count = 0
+
+for char in text:
+    if char.isalpha():
+        if char in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+
+print("Number of vowels:", vowel_count)
+print("Number of consonants:", consonant_count)
