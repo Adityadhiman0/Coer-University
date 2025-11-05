252d044aae62f3d545b0002d751e92527ba209c2
Adityadhiman0 <81180893+Adityadhiman0@users.noreply.github.com>
2025-11-05 17:26:34 +0530

    Check if two dictionaries are equal.py

---
+++ Python/Laboratory/Sheet 04/26. Check if two dictionaries are equal.py
@@ -0,0 +1,9 @@
+# Define two dictionaries
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 2, 'a': 1, 'c': 3}
+
+# Check equality
+if dict1 == dict2:
+    print("The dictionaries are equal.")
+else:
+    print("The dictionaries are not equal.")
